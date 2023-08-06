5105998eb2c52b3a725ea5fe5558d88b91b76287
Burak Cakir <cakirburak43@hotmail.com>
2023-08-07 00:03:40 +0300

    db connection and tool output are modified

--- src/components/toolcomponent.js
+++ src/components/toolcomponent.js
@@ -1,40 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-function ToolComponent(props) {
+function ToolComponent({classes,text,setIsChecked}) {
 
-  const [text, setText] = useState('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.');
+  const [namedEntities, setNamedEntities] = useState([]);
+  const [selectedText, setSelectedText] = useState('');
   const [selectedClass, setSelectedClass] = useState('');
-
-  const classes = props.classes;
 
   var entities = "";
   if(selectedClass !== null && selectedClass !== undefined) {
     entities = selectedClass.entities;
   }
 
-  const handleTextChange = (e) => {
-    setText(e.target.value);
-  };
-
   const handleClassSelect = (className) => {
     setSelectedClass(className);
-    alert("changed to " + className.classType);
   };
 
   const handleSubmit = (e) => {
-    //console.log(`Selected text: ${window.getSelection().toString()}`);
     e.preventDefault();
-    console.log(text);
-    console.log(selectedClass);
-    // Perform classification logic with 'text' and 'selectedClass'
-    // Reset the text and selectedClass state
-    setText('');
+    var output = {
+      text: text,
+      classType: selectedClass,
+      namedEntities: namedEntities
+    }
+    console.log(output);
+    
+    setIsChecked();
+
+    setSelectedText('');
     setSelectedClass('');
   };
 
   const handleEntitySet = (e) => {
     e.preventDefault();
-    console.log(window.getSelection().toString() + ", Entity set to " + e.target.textContent);
+    var entity = e.target.textContent;
+    var selectedText = window.getSelection().toString();
+    // console.log(window.getSelection()+ ", Entity set to " + e.target.textContent);
+    var start = text.indexOf(selectedText);
+    var end = start + selectedText.length;
+    setNamedEntities([...namedEntities, {text:selectedText, entity:entity, indexStart:start, indexEnd:end}]);
   };
 
   return (
@@ -42,12 +45,12 @@ function ToolComponent(props) {
       <form onSubmit={handleSubmit} className="h-100">
         <div className="d-flex flex-column h-100">
           <div className="w-100 h-50 mb-3">
-            <textarea className="w-100 h-100 fs-5" type="text" id="text-input" value={text} onChange={handleTextChange} />
+            <textarea className="w-100 h-100 fs-5" type="text" id="text-input" value={text}/>
           </div>
           <div className="mt-3 d-flex flex-column align-items-center">
-            <div className="w-100 text-start fs-5 ms-4">Entities</div>
+            {entities && <div className="w-100 text-start fs-5 ms-4">Entities</div>}
             <div className="d-flex flex-wrap w-100 mt-2">
-              {entities && entities.map((entity) => <button id="entityButtons" type="button" class="btn m-2 btn-success" onClick={handleEntitySet}>{entity}</button>)}
+              {entities && entities.map((entity) => <button id="entityButtons" type="button" className="btn m-2 btn-success" onClick={handleEntitySet}>{entity}</button>)}
             </div>
           </div>
           <div className="mt-3 d-flex flex-column align-items-center">
--- src/tool.js
+++ src/tool.js
@@ -2,13 +2,22 @@ import TextCardComponent from './components/textcardcomponent'
 import ToolComponent from './components/toolcomponent'
 import React, { useState, useEffect } from 'react';
 import useFetch from './useFetch';
+import { useRef } from 'react';
 
 function Tool() {
 
   const { data: texts, isPending: isTextPending, error: textError } = useFetch('http://localhost:8000/texts');
-  const { data: classes, isPending: isClassesPending, error: classesError } = useFetch('http://localhost:8001/classes');
-  
-  const entities = [];
+  const { data: classes, isPending: isClassesPending, error: classesError } = useFetch('http://localhost:8000/classes');
+  const [text, setText] = useState('');
+
+  const childRef = useRef();
+
+  const wrapperFunction = () => {
+    // check that the ref exists to avoid errors
+    if (!childRef.current) return;
+
+    childRef.current.instanceMethod();
+  }
 
   return (
     <div className="container-fluid min-vh-100 d-flex flex-column">
@@ -16,11 +25,11 @@ function Tool() {
       <div className="row mx-3 text-center">
         <div className="col-3 bg-light mx-2 py-4">
           {isTextPending && <div> Loading </div>}
-          {texts && texts.map((text) => <TextCardComponent text={text}/>) }
+          {texts && texts.map((text) => <TextCardComponent text={text} setTemp={setText} ref={childRef}/>) }
         </div>
         <div className="col bg-light mx-2 py-4" style={{ height: '685px'}}>
           {isClassesPending && <div> Loading </div>}
-          {classes && <ToolComponent classes={classes}/>}
+          {classes && <ToolComponent classes={classes} text={text} setIsChecked={wrapperFunction}/>}
         </div>
         <div className="col-2 bg-light mx-2">
           <h1 className="mb-4">Stats</h1>
