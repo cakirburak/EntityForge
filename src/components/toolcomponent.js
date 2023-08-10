832549877946e454aeb71cb4f76d3ca1f148e419
Baturalp Karslioglu <b.karslioglu@student.tudelft.nl>
2023-08-10 16:53:57 +0200

    assiging colors to different entities

--- src/components/toolcomponent.js
+++ src/components/toolcomponent.js
@@ -1,10 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 
 function ToolComponent({classes,text,setIsChecked}) {
 
   const [namedEntities, setNamedEntities] = useState([]);
   const [selectedText, setSelectedText] = useState('');
   const [selectedClass, setSelectedClass] = useState('');
+
+  const contentEditableRef = useRef(null);
+
+  const entityColors = {
+    date: "#ffee58",
+    time: "#64b5f6",
+    place: "#336BFF",
+    person: "#36FF33",
+    PNR: "#FF33C7",
+    fee: "#BCCD10",
+    reason: "#E6610A",
+    upadated: "#0AD2E6",
+  };
 
   var entities = "";
   if(selectedClass !== null && selectedClass !== undefined) {
@@ -28,16 +42,27 @@ function ToolComponent({classes,text,setIsChecked}) {
 
     setSelectedText('');
     setSelectedClass('');
+
+    contentEditableRef.current.innerHTML = text;
   };
 
   const handleEntitySet = (e) => {
     e.preventDefault();
     var entity = e.target.textContent;
     var selectedText = window.getSelection().toString();
-    // console.log(window.getSelection()+ ", Entity set to " + e.target.textContent);
+    console.log(window.getSelection()+ ", Entity set to " + e.target.textContent);
     var start = text.indexOf(selectedText);
     var end = start + selectedText.length;
+
+    const entityColor = entityColors[entity] || "#ffffff";
+
     setNamedEntities([...namedEntities, {text:selectedText, entity:entity, indexStart:start, indexEnd:end}]);
+
+    const contentEditableDiv = document.getElementById("content-editable");
+    const range = window.getSelection().getRangeAt(0);
+    const span = document.createElement("span");
+    span.style.backgroundColor = entityColor; // Change this to your desired background color
+    range.surroundContents(span);
   };
 
   return (
@@ -45,7 +70,13 @@ function ToolComponent({classes,text,setIsChecked}) {
       <form onSubmit={handleSubmit} className="h-100">
         <div className="d-flex flex-column h-100">
           <div className="w-100 h-50 mb-3">
-            <textarea className="w-100 h-100 fs-5" type="text" id="text-input" value={text}/>
+            <div
+              id="content-editable"
+              ref={contentEditableRef}
+              className="w-100 h-100 fs-5"
+              contentEditable
+              dangerouslySetInnerHTML={{ __html: text }}
+            />
           </div>
           <div className="mt-3 d-flex flex-column align-items-center">
             {entities && <div className="w-100 text-start fs-5 ms-4">Entities</div>}
